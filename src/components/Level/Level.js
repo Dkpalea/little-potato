d7b3d78490b4e168f299e71076117cd0c8afdab2
Dkpalea <dkpalea@ucsc.edu>
2021-01-25 21:13:46 -1000

    Implement answer check, begin level styles.

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -1,5 +1,30 @@
 import { Component } from 'react';
 import db from '../../db';
+import bacon from '../../assets/ingredient/bacon.png';
+import butter from '../../assets/ingredient/butter.png';
+import cheese from '../../assets/ingredient/cheese.png';
+import chives from '../../assets/ingredient/chives.png';
+import egg from '../../assets/ingredient/egg.png';
+import mayo from '../../assets/ingredient/mayo.png';
+import milk from '../../assets/ingredient/milk.png';
+import oil from '../../assets/ingredient/oil.png';
+import pepper from '../../assets/ingredient/pepper.png';
+import pepper_salad from '../../assets/ingredient/pepper_salad.png';
+import potato1 from '../../assets/ingredient/potato1.png';
+import potato2 from '../../assets/ingredient/potato2.png';
+import potato3 from '../../assets/ingredient/potato3.png';
+import potato4 from '../../assets/ingredient/potato4.png';
+import potato5 from '../../assets/ingredient/potato5.png';
+import salt1 from '../../assets/ingredient/salt1.png';
+import salt2 from '../../assets/ingredient/salt2.png';
+import vegetables from '../../assets/ingredient/vegetables.png';
+
+import level1 from '../../assets/level/level_1.png'
+import level2 from '../../assets/level/level_2.png'
+import level3 from '../../assets/level/level_3.png'
+import level4 from '../../assets/level/level_4.png'
+import level5 from '../../assets/level/level_5.png'
+
 
 import Recipe from '../Recipe/Recipe';
 
@@ -7,7 +32,15 @@ class Level extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {...this.generateQuestions(props.levelN)};
+    this.state = {
+      ...this.generateQuestions(),
+      userAnswer1: null,
+      userAnswer2: null,
+      userAnswer3: null,
+      userAnswer1IsCorrect: null,
+      userAnswer2IsCorrect: null,
+      userAnswer3IsCorrect: null,
+    };
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -142,27 +175,114 @@ class Level extends Component {
     );
   };
 
+  recipes = {
+    1: {
+      recipeName: 'French Fries',
+      ingredientLabel1: 'potatoes',
+      ingredientLabel2: 'salt',
+      ingredientLabel3: 'oil',
+      ingredientImgPath1: potato1,
+      ingredientImgPath2: salt1,
+      ingredientImgPath3: oil,
+      backgroundImgPath: level1,
+    },
+    2: {
+      recipeName: 'Potato Chips',
+      ingredientLabel1: 'potatoes',
+      ingredientLabel2: 'salt',
+      ingredientLabel3: 'pepper',
+      ingredientImgPath1: potato2,
+      ingredientImgPath2: salt2,
+      ingredientImgPath3: pepper,
+      backgroundImgPath: level2,
+    },
+    3: {
+      recipeName: 'Mashed Potatoes',
+      ingredientLabel1: 'potatoes',
+      ingredientLabel2: 'milk',
+      ingredientLabel3: 'butter',
+      ingredientImgPath1: potato3,
+      ingredientImgPath2: milk,
+      ingredientImgPath3: butter,
+      backgroundImgPath: level3,
+    },
+    4: {
+      recipeName: 'Baked Potatoes',
+      ingredientLabel1: 'potatoes',
+      ingredientLabel2: 'bacon',
+      ingredientLabel3: 'chives',
+      ingredientImgPath1: potato4,
+      ingredientImgPath2: bacon,
+      ingredientImgPath3: chives,
+      backgroundImgPath: level4,
+    },
+    5: {
+      recipeName: 'Baked Potatoes',
+      ingredientLabel1: 'potatoes',
+      ingredientLabel2: 'egg',
+      ingredientLabel3: 'mayo',
+      ingredientImgPath1: potato5,
+      ingredientImgPath2: egg,
+      ingredientImgPath3: mayo,
+      backgroundImgPath: level5,
+    },
+  };
+
+  checkAnswers = () => {
+    const userAnswer1IsCorrect = parseInt(document.getElementById('user-answer-1-input').value) === this.state.q1Answer;
+    const userAnswer2IsCorrect = parseInt(document.getElementById('user-answer-2-input').value) === this.state.q2Answer;
+    const userAnswer3IsCorrect = parseInt(document.getElementById('user-answer-3-input').value) === this.state.q3Answer;
+    this.setState({userAnswer1IsCorrect, userAnswer2IsCorrect, userAnswer3IsCorrect});
+
+    if (userAnswer1IsCorrect && userAnswer2IsCorrect && userAnswer3IsCorrect) {
+      this.props.increaseLevelNumber();
+    }
+  };
+
   render() {
     return (
       <div className="level">
-        <h1>Level {this.props.levelNumber}</h1>
-        <Recipe
-          levelNumber={this.props.levelNumber}
-          randNum1={this.state.randNum1}
-          randNum2={this.state.randNum2}
-          q1Answer={this.state.q1Answer}
-          randNum3={this.state.randNum3}
-          randNum4={this.state.randNum4}
-          q2Answer={this.state.q2Answer}
-          randNum5={this.state.randNum5}
-          randNum6={this.state.randNum6}
-          q3Answer={this.state.q3Answer}
-          randNumA={this.state.randNumA}
-          randNumB={this.state.randNumB}
-          randNumC={this.state.randNumC}
-        />
-        <button onClick={() => this.props.increaseLevelNumber()}>next</button>
-
+        {/*<div className="level-background" style={{backgroundImage: `url(${this.recipes[this.props.levelNumber].backgroundImgPath})`}} />*/}
+        <div className="level-inner">
+          <div className="left-col">
+            <div>Level {this.props.levelNumber} of 5</div>
+            <Recipe
+              levelNumber={this.props.levelNumber}
+              randNum1={this.state.randNum1}
+              randNum2={this.state.randNum2}
+              q1Answer={this.state.q1Answer}
+              randNum3={this.state.randNum3}
+              randNum4={this.state.randNum4}
+              q2Answer={this.state.q2Answer}
+              randNum5={this.state.randNum5}
+              randNum6={this.state.randNum6}
+              q3Answer={this.state.q3Answer}
+              randNumA={this.state.randNumA}
+              randNumB={this.state.randNumB}
+              randNumC={this.state.randNumC}
+            />
+            {/*<img className="level-background" src={this.recipes[this.props.levelNumber].backgroundImgPath} />*/}
+          </div>
+          <div className="right-col">
+            <div className="right-col-top">
+              <div className="user-answer">
+                <img className="user-answer-img" src={this.recipes[this.props.levelNumber].ingredientImgPath1} />
+                <input id="user-answer-1-input" type="number" min="0" style={(this.state.userAnswer1IsCorrect === false)?{border: '3px solid red'}:null} />
+              </div>
+              <div className="user-answer">
+                <img className="user-answer-img" src={this.recipes[this.props.levelNumber].ingredientImgPath2} />
+                <input id="user-answer-2-input" type="number" min="0" style={(this.state.userAnswer2IsCorrect === false)?{border: '3px solid red'}:null} />
+              </div>
+              <div className="user-answer">
+                <img className="user-answer-img" src={this.recipes[this.props.levelNumber].ingredientImgPath3} />
+                <input id="user-answer-3-input" type="number" min="0" style={(this.state.userAnswer3IsCorrect === false)?{border: '3px solid red'}:null} />
+              </div>
+            </div>
+            <div className="right-col-bottom">
+              <button onClick={() => this.checkAnswers()} />
+            </div>
+            </div>
+        </div>
       </div>
     );
   }
--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -6,60 +6,60 @@ class Recipe extends Component {
       case 1:
         return (
           <div className="recipe">
-            <h1>Make french fries using...</h1>
-            <h2>{this.props.randNum1} + {this.props.randNum2} potatoes</h2>
+            <div>Make french fries using...</div>
+            <div>{this.props.randNum1} + {this.props.randNum2} potatoes</div>
             <span>Answer: {this.props.q1Answer}</span>
-            <h2>{this.props.randNum3} + {this.props.randNum4} pinches of salt</h2>
+            <div>{this.props.randNum3} + {this.props.randNum4} pinches of salt</div>
             <span>Answer: {this.props.q2Answer}</span>
-            <h2>{this.props.randNum5} + {this.props.randNum6} ounces of oil</h2>
+            <div>{this.props.randNum5} + {this.props.randNum6} ounces of oil</div>
             <span>Answer: {this.props.q3Answer}</span>
           </div>
         );
       case 2:
         return (
           <div className="recipe">
-            <h1>Make potato chips using...</h1>
-            <h2>{this.props.randNum1} - {this.props.randNum2} potatoes</h2>
+            <div>Make potato chips using...</div>
+            <div>{this.props.randNum1} - {this.props.randNum2} potatoes</div>
             <span>Answer: {this.props.q1Answer}</span>
-            <h2>{this.props.randNum3} - {this.props.randNum4} pinches of salt</h2>
+            <div>{this.props.randNum3} - {this.props.randNum4} pinches of salt</div>
             <span>Answer: {this.props.q2Answer}</span>
-            <h2>{this.props.randNum5} - {this.props.randNum6} pinches of pepper</h2>
+            <div>{this.props.randNum5} - {this.props.randNum6} pinches of pepper</div>
             <span>Answer: {this.props.q3Answer}</span>
           </div>
         );
       case 3:
         return (
           <div className="recipe">
-            <h1>Make mashed potatoes using...</h1>
-            <h2>{this.props.randNum1} x {this.props.randNum2} potatoes</h2>
+            <div>Make mashed potatoes using...</div>
+            <div>{this.props.randNum1} x {this.props.randNum2} potatoes</div>
             <span>Answer: {this.props.q1Answer}</span>
-            <h2>{this.props.randNum3} x {this.props.randNum4} cups of milk</h2>
+            <div>{this.props.randNum3} x {this.props.randNum4} cups of milk</div>
             <span>Answer: {this.props.q2Answer}</span>
-            <h2>{this.props.randNum5} x {this.props.randNum6} sticks of butter</h2>
+            <div>{this.props.randNum5} x {this.props.randNum6} sticks of butter</div>
             <span>Answer: {this.props.q3Answer}</span>
           </div>
         );
       case 4:
         return (
           <div className="recipe">
-            <h1>Make baked potatoes using...</h1>
-            <h2>{this.props.randNum1} ÷ {this.props.randNum2} potatoes</h2>
+            <div>Make baked potatoes using...</div>
+            <div>{this.props.randNum1} ÷ {this.props.randNum2} potatoes</div>
             <span>Answer: {this.props.q1Answer}</span>
-            <h2>{this.props.randNum3} ÷ {this.props.randNum4} cups of milk</h2>
+            <div>{this.props.randNum3} ÷ {this.props.randNum4} pieces of bacon</div>
             <span>Answer: {this.props.q2Answer}</span>
-            <h2>{this.props.randNum5} ÷ {this.props.randNum6} sticks of butter</h2>
+            <div>{this.props.randNum5} ÷ {this.props.randNum6} sprigs of chives</div>
             <span>Answer: {this.props.q3Answer}</span>
           </div>
         );
       case 5:
         return (
           <div className="recipe">
-            <h1>Make potato salad using...</h1>
-            <h2>{this.props.randNumA} - {this.props.randNum1} + {this.props.randNum2} potatoes</h2>
+            <div>Make potato salad using...</div>
+            <div>{this.props.randNumA} - {this.props.randNum1} + {this.props.randNum2} potatoes</div>
             <span>Answer: {this.props.q1Answer}</span>
-            <h2>{this.props.randNumB} - {this.props.randNum3} ÷ {this.props.randNum4} cups of milk</h2>
+            <div>{this.props.randNumB} - {this.props.randNum3} ÷ {this.props.randNum4} cups of milk</div>
             <span>Answer: {this.props.q2Answer}</span>
-            <h2>{this.props.randNumC} x {this.props.randNum5} ÷ {this.props.randNum6} sticks of butter</h2>
+            <div>{this.props.randNumC} x {this.props.randNum5} ÷ {this.props.randNum6} sticks of butter</div>
             <span>Answer: {this.props.q3Answer}</span>
           </div>
         );
